35048e2aade777b353533abab770e6f9c3109197
AnhNTSE183225 <anhntse183225@fpt.edu.vn>
2024-05-28 00:01:47 +0700

    update navbar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,13 +1,46 @@
 import { Link } from 'react-router-dom';
-import './NavigationBar.css'
+import './NavigationBar.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
 const NavigationBar = () => {
     return (
-        <div className='navbar'>
-            <Link to='/'>Home</Link>
-            <Link to='/diamond-price-list'>Diamond price list</Link>
-            <Link to='/custom-design'>Request a custom design</Link>
-        </div>
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">LOGO</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Moc Nguyen
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">View profile</a></li>
+                                    <li><a className="dropdown-item" href="#">Your requests</a></li>
+                                    <li><a className="dropdown-item" href="#">Your designs</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <Link className='nav-link' to='/'>Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className='nav-link' to='/diamond-price-list'>Diamond price list</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className='nav-link' to='/custom-design'>Request a custom design</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
     )
 }
 
