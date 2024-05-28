0f0f254dfd6635adde919cc6b02fcbcf0d81809c
AnhNTSE183225 <anhntse183225@fpt.edu.vn>
2024-05-28 16:13:24 +0700

    conflict test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
         <div>
             <NavigationBar/>
             <div>
-                <h1>Home page</h1>
+                <h1>Home page 1</h1>
             </div>
         </div>
     )
