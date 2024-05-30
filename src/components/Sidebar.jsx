36a970a9001ce0013230225db9fd2165ea31a29b
AnhNTSE183225 <anhntse183225@fpt.edu.vn>
2024-05-30 15:23:17 +0700

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,67 +3,59 @@ import '../../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
 const Sidebar = () => {
     return (
-        <div className="flex-shrink-0 p-3 bg-white" style={{width: "320px"}}>
-            <a href="/" className="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
-                <svg className="bi me-2" width="30" height="24"><use xlink:href="#bootstrap" /></svg>
-                <span className="fs-5 fw-semibold">Collapsible</span>
+        <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark">
+            <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap" /></svg>
+                <span className="fs-4">Sidebar</span>
             </a>
-            <ul className="list-unstyled ps-0">
-                <li className="mb-1">
-                    <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+            <hr />
+            <ul className="nav nav-pills flex-column mb-auto">
+                <li className="nav-item">
+                    <a href="#" className="nav-link active" aria-current="page">
+                        <svg className="bi me-2" width="16" height="16"><use xlinkHref="#home" /></svg>
                         Home
-                    </button>
-                    <div className="collapse show" id="home-collapse">
-                        <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" className="link-dark rounded">Overview</a></li>
-                            <li><a href="#" className="link-dark rounded">Updates</a></li>
-                            <li><a href="#" className="link-dark rounded">Reports</a></li>
-                        </ul>
-                    </div>
+                    </a>
                 </li>
-                <li className="mb-1">
-                    <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+                <li>
+                    <a href="#" className="nav-link text-white">
+                        <svg className="bi me-2" width="16" height="16"><use xlinkHref="#speedometer2" /></svg>
                         Dashboard
-                    </button>
-                    <div className="collapse" id="dashboard-collapse">
-                        <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" className="link-dark rounded">Overview</a></li>
-                            <li><a href="#" className="link-dark rounded">Weekly</a></li>
-                            <li><a href="#" className="link-dark rounded">Monthly</a></li>
-                            <li><a href="#" className="link-dark rounded">Annually</a></li>
-                        </ul>
-                    </div>
+                    </a>
                 </li>
-                <li className="mb-1">
-                    <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+                <li>
+                    <a href="#" className="nav-link text-white">
+                        <svg className="bi me-2" width="16" height="16"><use xlinkHref="#table" /></svg>
                         Orders
-                    </button>
-                    <div className="collapse" id="orders-collapse">
-                        <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" className="link-dark rounded">New</a></li>
-                            <li><a href="#" className="link-dark rounded">Processed</a></li>
-                            <li><a href="#" className="link-dark rounded">Shipped</a></li>
-                            <li><a href="#" className="link-dark rounded">Returned</a></li>
-                        </ul>
-                    </div>
+                    </a>
                 </li>
-                <li className="border-top my-3"></li>
-                <li className="mb-1">
-                    <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-                        Account
-                    </button>
-                    <div className="collapse" id="account-collapse">
-                        <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" className="link-dark rounded">New...</a></li>
-                            <li><a href="#" className="link-dark rounded">Profile</a></li>
-                            <li><a href="#" className="link-dark rounded">Settings</a></li>
-                            <li><a href="#" className="link-dark rounded">Sign out</a></li>
-                        </ul>
-                    </div>
+                <li>
+                    <a href="#" className="nav-link text-white">
+                        <svg className="bi me-2" width="16" height="16"><use xlinkHref="#grid" /></svg>
+                        Products
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="nav-link text-white">
+                        <svg className="bi me-2" width="16" height="16"><use xlinkHref="#people-circle" /></svg>
+                        Customers
+                    </a>
                 </li>
             </ul>
+            <hr />
+            <div className="dropdown">
+                <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2" />
+                    <strong>mdo</strong>
+                </a>
+                <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+                    <li><a className="dropdown-item" href="#">New project...</a></li>
+                    <li><a className="dropdown-item" href="#">Settings</a></li>
+                    <li><a className="dropdown-item" href="#">Profile</a></li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li><a className="dropdown-item" href="#">Sign out</a></li>
+                </ul>
+            </div>
         </div>
-
     )
 }
 
