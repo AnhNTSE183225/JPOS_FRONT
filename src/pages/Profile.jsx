2e53b8be4d170359ebd5b1992811f3c027e70520
AnhNTSE183225 <anhntse183225@fpt.edu.vn>
2024-05-30 15:13:51 +0700

    sidebar test

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,17 @@
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import NavigatonBar from '../components/NavigationBar'
+import Sidebar from '../components/Sidebar';
 
 const ProfilePage = () => {
     return (
         <>
             <NavigatonBar />
-            <div className='container'>
+            <div className='container-fluid'>
                 <div className='row'>
+                    <div className='col'>
+                        <Sidebar />
+                    </div>
                     <div className='col'>
                         <h1>Liem</h1>
                     </div>
