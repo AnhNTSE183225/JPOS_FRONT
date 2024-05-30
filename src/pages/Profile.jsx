dc3f1e9c225645d486f4dea7b387394e24afc6db
Liem <LiemNHTSE183715@fpt.edu.vn>
2024-05-31 00:18:44 +0700

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,20 +1,31 @@
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../../node_modules/bootstrap/dist/js/bootstrap.bundle';
-import NavigatonBar from '../components/NavigationBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import NavigationBar from '../components/NavigationBar';
 import Sidebar from '../components/Sidebar';
 
 const ProfilePage = () => {
     return (
         <>
+            <NavigationBar />
             <div className='container-fluid p-0'>
                 <div className='row'>
                     <div className='col-md-2'>
-                        Test
+                        <Sidebar />
+                    </div>
+                    <div className='col-md-10'>
+                        <div className="card" style={{ width: '400px' }}>
+                            <img className="card-img-top" src="img_avatar1.png" alt="Card image" />
+                            <div className="card-body">
+                                <h4 className="card-title">John Doe</h4>
+                                <p className="card-text">Some example text.</p>
+                                <a href="#" className="btn btn-primary">See Profile</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
 export default ProfilePage;
