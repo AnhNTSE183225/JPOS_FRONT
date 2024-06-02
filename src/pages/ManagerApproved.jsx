db0ea65f1ba2997a5fd5005a16b6030bbf408c86
Nguyen <nguyenmse181820@fpt.edu.vn>
2024-06-03 00:19:34 +0700

    Complete ManagerApproved

--- src/pages/ManagerApproved.jsx
+++ src/pages/ManagerApproved.jsx
@@ -1,16 +1,41 @@
 import { useNavigate } from "react-router-dom";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import { formatDate, formatPrice } from '../helper_function/ConvertFunction'
+import React, { useEffect, useState } from 'react';
+import { Toaster, toast } from 'sonner';
+import axios from 'axios';
+
 
 const ManagerApproved = ({ order }) => {
-  const navigate = useNavigate;
-  const forwardQuota = () => {};
+
+  const navigate = useNavigate();
+
+  const forwardQuotation = () => {
+    let staff_id = sessionStorage.getItem('staff_id');
+    if (staff_id !== null) {
+        axios.post(`http://localhost:8080/api/${order.id}/forward-quotation`)
+            .then(
+                response => {
+                    toast(response.data);
+                    navigate('/profile/request');
+                }
+            ).catch(
+                error => {
+                    console.log(error);
+                }
+            )
+    } else {
+        toast('Logged out');
+        navigate('/login');
+    }
+}
 
   return (
     <>
       <div className="container-fluid">
-        <div className="row mt-5">
-          <div className="mb-3">
+        <div className="row mt-3">
+          <div className="mb-2">
             <h3>
               <b>Forward Quotation</b>
             </h3>
@@ -29,19 +54,93 @@ const ManagerApproved = ({ order }) => {
             </p>
             <p className="px-3">{order.budget}</p>
           </div>
-          <div className="col-md-8 px-3">
-            <p>
-              <b>Diamond</b>
-            </p>
-            <p className="px-3">{order.product.diamonds.diamondId}</p>
-            <p>
-              <b>Description</b>
-            </p>
-            <p className="px-3">{order.description}</p>
-            <p>
-              <b>Budget</b>
-            </p>
-            <p className="px-3">{order.budget}</p>
+          <div className="col-md-6 px-2">
+            <div className='row'>
+              <p><b>Product ID: </b> <b>{order.product.productId}</b></p>
+              <p>
+                <b>
+                  Chosen diamonds
+                </b>
+              </p>
+              <div className='col'>
+                <table className='table table-hover text-center'>
+                  <thead>
+                    <tr>
+                      <th>Num</th>
+                      <th>ID</th>
+                      <th>Code</th>
+                      <th>Cut</th>
+                      <th>Color</th>
+                      <th>Clarity</th>
+                      <th>Carat Weight</th>
+
+                    </tr>
+                  </thead>
+                  <tbody className='table-group-divider'>
+                    {order.product.diamonds.map((diamond, index) => (
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{diamond.diamondId}</td>
+                        <td>{diamond.diamondCode}</td>
+                        <td>{diamond.cut}</td>
+                        <td>{diamond.color}</td>
+                        <td>{diamond.clarity}</td>
+                        <td>{diamond.caratWeight}</td>
+
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+
+              <div className="d-flex justify-content-between align-items-center">
+                <p className="fw-semibold">Diamond price: {formatPrice(order.qdiamondPrice + order.ediamondPrice)}</p>
+              </div>
+
+              <div className='col'>
+                <table className='table table-hover'>
+                  <thead>
+                    <tr>
+                      <th>Num</th>
+                      <th>ID</th>
+                      <th>Material Name</th>
+                      <th>Weight</th>
+                    </tr>
+                  </thead>
+                  <tbody className='table-group-divider'>
+                    {order.product.materials.map(
+                      (material, index) => (
+                        <tr key={index}>
+                          <td>{index + 1}</td>
+                          <td>{material.material.materialId}</td>
+                          <td>{material.material.materialName}</td>
+                          <td>{material.weight}</td>
+                        </tr>
+                      )
+                    )}
+                  </tbody>
+                </table>
+              </div>
+
+              <div className="d-flex justify-content-between align-items-center">
+                <p className="fw-semibold">Material price: {formatPrice(order.qmaterialPrice + order.ematerialPrice)}</p>
+              </div>
+
+              <div>
+                <p className="fw-semibold">Extra diamond: {formatPrice(order.ediamondPrice)}</p>
+                <p className="fw-semibold">Extra material: {formatPrice(order.ematerialPrice)}</p>
+              </div>
+              <div>
+                <p className="fw-semibold">Production: {formatPrice(order.productionPrice + ((order.qdiamondPrice + order.ediamondPrice + order.qmaterialPrice + order.ematerialPrice + order.ediamondPrice + order.ematerialPrice) * order.markupRate) - (order.qdiamondPrice + order.ediamondPrice + order.qmaterialPrice + order.ematerialPrice + order.ediamondPrice + order.ematerialPrice))}</p>
+              </div>
+              <hr />
+              <div>
+                <p><b>Total Price: {formatPrice(order.qdiamondPrice + order.ediamondPrice + order.qmaterialPrice + order.ematerialPrice + order.ediamondPrice + order.ematerialPrice + order.productionPrice + ((order.qdiamondPrice + order.ediamondPrice + order.qmaterialPrice + order.ematerialPrice + order.ediamondPrice + order.ematerialPrice) * order.markupRate) - (order.qdiamondPrice + order.ediamondPrice + order.qmaterialPrice + order.ematerialPrice + order.ediamondPrice + order.ematerialPrice))}</b></p>
+              </div>
+              <div className='col'>
+                        <button onClick={forwardQuotation} className='btn btn-success w-100'>Forward to {order.customer.name}</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
