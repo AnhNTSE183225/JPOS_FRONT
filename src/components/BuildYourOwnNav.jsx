dde25a3746671ecf6cb9ad1ef974b4eef50e7177
AnhNTSE183225 <anhntse183225@fpt.edu.vn>
2024-06-03 23:02:10 +0700

    Add links to nav build your own, add navigation to build your own

--- src/components/BuildYourOwnNav.jsx
+++ src/components/BuildYourOwnNav.jsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import { useState } from 'react';
 
 const BuildYourOwnNav = () => {
+
     return (
-        <>
-            <Link to="/build-your-own/choose-diamond">Link</Link>
+        <>            
+        <Link to="/build-your-own/choose-diamond">Choose diamond</Link>
+            <Link to="/build-your-own/choose-setting">Choose diamond</Link>
+            <Link to="/build-your-own/complete-product">Choose diamond</Link>
+            <p>
+                This is navbar
+            </p>
         </>
     );
 }
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -63,6 +63,9 @@ const NavigationBar = () => {
                             <li className="nav-item">
                                 <Link className='nav-link' to='/diamond-price-list'>Diamond price list</Link>
                             </li>
+                            <li className='nav-item'>
+                                <Link className='nav-link' to="/build-your-own/choose-diamond">Build your own</Link>
+                            </li>
                             <li className="nav-item">
                                 <Link className='nav-link' to='/custom-design'>Request a custom design</Link>
                             </li>
