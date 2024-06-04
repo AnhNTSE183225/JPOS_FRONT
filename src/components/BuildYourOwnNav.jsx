46bf79aaadeedde5f89084645a796ff397dd3dd9
Nguyen <nguyenmse181820@fpt.edu.vn>
2024-06-04 23:31:04 +0700

    add image design order

--- src/components/BuildYourOwnNav.jsx
+++ src/components/BuildYourOwnNav.jsx
@@ -2,25 +2,57 @@ import { Link } from 'react-router-dom';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import './BuildYourOwnNav.css'; // Create a CSS file for additional styling
+import dia1 from "../assets/Diamond1.jpg";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 const BuildYourOwnNav = () => {
+    const [diamondPriceList, setDiamondPriceList] = useState([]);
+
+    const fetchDiamondList = async () => {
+        try {
+            const response = await axios.get('http://localhost:8080/api/diamond-prices');
+            if (response.status === 204) {
+                console.log("No data");
+            } else {
+                setDiamondPriceList(response.data);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchDiamondList();
+    }, []);
     return (
-        <div className="build-your-own-nav">
-            <div className="nav-steps">
-                <Link to="/build-your-own/choose-diamond" className="nav-link step active">
-                    <span className="step-number">1</span>
-                    <span className="step-label">Choose a Diamond</span>
-                </Link>
-                <Link to="/build-your-own/choose-setting" className="nav-link step">
-                    <span className="step-number">2</span>
-                    <span className="step-label">Choose a Setting</span>
-                </Link>
-                <Link to="/build-your-own/complete-product" className="nav-link step">
-                    <span className="step-number">3</span>
-                    <span className="step-label">Complete Ring</span>
-                </Link>
+        <div className="container mt-4">
+            <Link to="/build-your-own/choose-diamond">
+                <button type="button" className="btn btn-light btn-lg col-4">Choose a Diamond</button>
+            </Link>
+            <Link to="/build-your-own/choose-setting">
+                <button type="button" className="btn btn-light btn-lg col-4">Choose a Setting</button>
+            </Link>
+            <Link to="/build-your-own/complete-product">
+                <button type="button" className="btn btn-light btn-lg col-4">Complete Ring</button>
+            </Link>
+            <div className="text-center mt-3">
+                <h4><b>Diamond Finder</b></h4>
             </div>
-            <p>This is navbar</p>
+            <div className="row">
+                <div className="col-md-3 col-sm-6">
+                    <img src={dia1} alt="diamond" className="img-fluid" />
+                </div>
+                <div className="col-md-3 col-sm-6">
+                    <img src={dia1} alt="diamond" className="img-fluid" />
+                </div>
+                <div className="col-md-3 col-sm-6">
+                    <img src={dia1} alt="diamond" className="img-fluid" />
+                </div>
+                <div className="col-md-3 col-sm-6">
+                    <img src={dia1} alt="diamond" className="img-fluid" />
+                </div>
+            </div>
         </div>
     );
 }
