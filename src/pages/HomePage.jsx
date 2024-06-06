abb13340b5eb09d2a129d3d90501f4e053ad04cc
Nguyen <nguyenmse181820@fpt.edu.vn>
2024-06-06 23:13:07 +0700

    clean homePage cmt confused css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,24 @@
 import '../css/HomePage.css';
+
 const HomePage = () => {
 
     return (
         <>
             <section className="home">
-                <div className="container">
-                    <video src="https://media2.bulgari.com/video/upload/f_auto,q_auto/v1716383679/homepage/brand-campaign/2024/bulgari_ER_Heromovie_10s_withoutbands_VI_16-9_4k_1.mp4" autoPlay loop muted playsInline></video>
-                </div>
-                <div className="overlay-text">
-                    <h1>ETERNALLY REBORN</h1>
+                <div className="overlay"></div>
+                <video src="https://media2.bulgari.com/video/upload/f_auto,q_auto/v1716383679/homepage/brand-campaign/2024/bulgari_ER_Heromovie_10s_withoutbands_VI_16-9_4k_1.mp4" autoPlay loop muted playsInline></video>
+                <div className="homeContent container">
+                    <div className="textDiv">
+                        <span className="smallText">
+                            Our campaign
+                        </span>
+                        <h1 className='homeTitle'>
+                            Reborn
+                        </h1>
+                    </div>
                 </div>
             </section>
+            
         </>
     );
 };
