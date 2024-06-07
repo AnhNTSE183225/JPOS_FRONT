f5839db39793c0c01f142d3cd71e5323b79b3280
Liem <LiemNHTSE183715@fpt.edu.vn>
2024-06-07 23:28:13 +0700

    settingspage

--- src/pages/build-your-own/SettingDetails.jsx
+++ src/pages/build-your-own/SettingDetails.jsx
@@ -3,8 +3,11 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "/src/css/SettingDetails.module.css"; 
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightLeft, faTruckFast } from '@fortawesome/free-solid-svg-icons';
+
 const SettingDetails = () => {
-    const designId = useParams().designId;
+    const { designId } = useParams();
     const [productDesign, setProductDesign] = useState(null);
     const [selectedShell, setSelectedShell] = useState(null);
     const [showShellDetails, setShowShellDetails] = useState(false);
@@ -25,7 +28,7 @@ const SettingDetails = () => {
 
     useEffect(() => {
         fetchData();
-    }, []);
+    }, [designId]);
 
     const handleShellClick = (shell) => {
         setSelectedShell(shell);
@@ -36,68 +39,70 @@ const SettingDetails = () => {
     };
 
     if (productDesign === null) {
-        return <div className="loading">Loading...</div>;
+        return <div className={styles.loading}>Loading...</div>;
     } else {
         return (
-            <div className={`${styles['container']}`} id={`${styles['setting-details']}`}>
-                <div className={`${styles['content']}`}>
-                    <div  className={`${styles['image-selection']}`}>
-                        <img src="path/to/your/ring-image.jpg" alt="Ring" />
+            <div className={styles.container} id={styles["setting-details"]}>
+                <div className={styles.content}>
+                    <div className={styles["image-section"]}>
+                        <img src="https://ion.bluenile.com/sets/Jewelry-bn/194386/RND/Images/LS_stage_0.jpg" alt="Ring" />
                     </div>
-                    <div className={`${styles['details-section']}`}>
-                        <h1 className={`${styles['title']}`}>{productDesign.designName}</h1>
-                        <h2 className={`${styles['subtitle']}`}>{selectedShell ? selectedShell.shellName : "Select a Shell"}</h2>
-                        <div className={`${styles['price']}`}>$640 (Setting Price)</div>
-                        <div className={`${styles['metal-type-section']}`}>
-                            <h3 className={`${styles['metal-type-title']}`}></h3>
-                            <div className={`${styles['shell-list']}`}>
+                    <div className={styles["details-section"]}>
+                        <h1 className={styles.title}>{productDesign.designName}</h1>
+                        <h2 className={styles.subtitle}>{selectedShell ? selectedShell.shellName : "Select a Shell"}</h2>
+                        <div className={styles.price}>$640 (Setting Price)</div>
+                        <div className={styles["metal-type-section"]}>
+                            <h3 className={styles["metal-type-title"]}></h3>
+                            <div className={styles["shell-list"]}>
                                 {productDesign.productShellDesigns.map(shell => (
                                     <div 
                                         key={shell.productShellDesignId}
-                                        className={`shell-item ${selectedShell && selectedShell.productShellDesignId === shell.productShellDesignId ? 'selected' : ''}`}
+                                        className={`${styles["shell-item"]} ${selectedShell && selectedShell.productShellDesignId === shell.productShellDesignId ? styles.selected : ''}`}
                                         onClick={() => handleShellClick(shell)}
                                     >
-                                        <span className="shell-label">{shell.shellName}</span>
+                                        <span className={styles["shell-label"]}>{shell.shellName}</span>
                                     </div>
                                 ))}
                             </div>
-                            <div className={`${styles['toggle-button']}`} onClick={toggleShellDetails}>
+                            <div className={styles["toggle-button"]} onClick={toggleShellDetails}>
                                 <h3>Shell Details</h3>
                                 <span>{showShellDetails ? '-' : '+'}</span>
                             </div>
                             {showShellDetails && selectedShell && (
-                                <div className="shell-details-section">
-                                    <h3 className="shell-title">{selectedShell.shellName} Details</h3>
-                                    <p className="shell-detail"><strong>ID:</strong> {selectedShell.productShellDesignId}</p>
-                                    <p className="shell-detail"><strong>Diamonds:</strong> {selectedShell.diamondQuantity}</p>
-                                    <p className="shell-detail"><strong>Production Price:</strong> ${selectedShell.productionPrice}</p>
-                                    <p className="shell-detail"><strong>Diamond Price:</strong> ${selectedShell.ediamondPrice}</p>
-                                    <p className="shell-detail"><strong>Material Price:</strong> ${selectedShell.ematerialPrice}</p>
+                                <div className={styles["shell-details-section"]}>
+                                    <h3 className={styles["shell-title"]}>{selectedShell.shellName} Details</h3>
+                                    <p className={styles["shell-detail"]}><strong>ID:</strong> {selectedShell.productShellDesignId}</p>
+                                    <p className={styles["shell-detail"]}><strong>Diamonds:</strong> {selectedShell.diamondQuantity}</p>
+                                    <p className={styles["shell-detail"]}><strong>Production Price:</strong> ${selectedShell.productionPrice}</p>
+                                    <p className={styles["shell-detail"]}><strong>Diamond Price:</strong> ${selectedShell.ediamondPrice}</p>
+                                    <p className={styles["shell-detail"]}><strong>Material Price:</strong> ${selectedShell.ematerialPrice}</p>
                                 </div>
                             )}
                         </div>
-                        <div  className={`${styles['payment-options']}`}>
-                            <div className={`${styles['option']}`}>
-                                <img src="path/to/payment-icon.png" alt="Payment" />
+                        <div className={styles["payment-options"]}>
+                            <div className={styles.option}>
+                                
                                 Flexible Payment Options: 3 Interest-Free Payments of $1,333
                             </div>
-                            <div className={`${styles['option']}`}>
-                                <img src="path/to/return-icon.png" alt="Returns" />
+                            <div className={styles.option}>
+                                
+                                <FontAwesomeIcon icon={faRightLeft} style={{ marginRight: '8px' }} />
                                 Free Returns: Our commitment to you does not end at delivery. We offer free returns (U.S and Canada) to make your experience as easy as possible.
                             </div>
-                            <div className={`${styles['option']}`}>
-                                <img src="path/to/shipping-icon.png" alt="Shipping" />
+                            <div className={styles.option}>
+                                
+                                <FontAwesomeIcon icon={faTruckFast} style={{ marginRight: '8px' }} />
                                 Free Shipping: We're committed to making your entire experience a pleasant one, from shopping to shipping.
                             </div>
                         </div>
-                        <button className={`${styles['button']}`}>Select this setting</button>
-                        <button className={`${styles['button secondary-button']}`}>Consult an expert</button>
-                        <div className={`${styles['product-details']}`} >
+                        <button className={styles.button}>Select this setting</button>
+                        <button className={`${styles.button} ${styles["secondary-button"]}`}>Consult an expert</button>
+                        <div className={styles["product-details"]}>
                             <h3>Product Details</h3>
-                            <p className={`${styles['detail-item']}`}>productDesignId: {productDesign.productDesignId}</p>
-                            <p className={`${styles['detail-item']}`}>designType: {productDesign.designType}</p>
-                            <p className={`${styles['detail-item']}`}>Width: 1.80mm</p>
-                            <p className={`${styles['detail-item']}`}>Rhodium Finish: Yes</p>
+                            <p className={styles["detail-item"]}>productDesignId: {productDesign.productDesignId}</p>
+                            <p className={styles["detail-item"]}>designType: {productDesign.designType}</p>
+                            <p className={styles["detail-item"]}>Width: 1.80mm</p>
+                            <p className={styles["detail-item"]}>Rhodium Finish: Yes</p>
                         </div>
                     </div>
                 </div>
