19c862d2c844616669739951cc8b2631c234bd96
AnhNTSE183225 <anhntse183225@fpt.edu.vn>
2024-06-10 02:19:19 +0700

    revamp CompleteProduct.jsx

--- src/helper_function/FetchPriceFunctions.jsx
+++ src/helper_function/FetchPriceFunctions.jsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const fetchMaterialPrice = async (id) => {
     try {
         const response = await axios.get(`http://localhost:8080/api/materialPrices/${id}`);
--- src/pages/build-your-own/CompleteProduct.jsx
+++ src/pages/build-your-own/CompleteProduct.jsx
@@ -5,6 +5,7 @@ import { Toaster, toast } from 'sonner';
 import { Link, useNavigate } from 'react-router-dom';
 import { formatDate, formatPrice } from '../../helper_function/ConvertFunction';
 import axios from 'axios';
+import { fetchDiamondPrice, fetchMaterialPrice } from '../../helper_function/FetchPriceFunctions';
 
 const CompleteProduct = () => {
 
@@ -15,18 +16,70 @@ const CompleteProduct = () => {
     const [shell, setShell] = useState(null);
     const [diamonds, setDiamonds] = useState([]);
     const [materials, setMaterials] = useState([]);
+    const [estimatedPrice, setEstimatedPrice] = useState(null);
 
     const [orderId, setOrderId] = useState(null);
     const [order, setOrder] = useState(null);
 
     useEffect(() => {
+        const getDesign = async () => {
+            if (productSetting.designId === null) {
+                toast.info(`Please return for selection`);
+                navigate("/build-your-own/choose-setting");
+            } else {
+                try {
+                    //console.log(`GET http://localhost:8080/api/product-designs/${productSetting.designId}`);
+                    const response = await axios.get(`http://localhost:8080/api/product-designs/${productSetting.designId}`);
+                    if (!response.data || response.status === 204) {
+                        console.log('error, cannot fetch, wrong id');
+                    } else {
+                        const shell = response.data.productShellDesigns.find(val => val.productShellDesignId === productSetting.shellId)
+                        const diamonds = await getDiamonds();
+                        const materials = await getMaterials();
+                        const price = await getEstimatePrice(shell, diamonds, materials);
+
+                        setProductDesign(response.data);
+                        setShell(shell);
+                        setDiamonds(diamonds);
+                        setMaterials(materials);
+                        setEstimatedPrice(price);
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
+
         getDesign();
-    },[])
+    }, [])
+
+    const getEstimatePrice = async (shell, diamonds, materials) => {
+        let totalPrice = 0;
+        //console.log(`Material price: ${shell.ematerialPrice}`);
+        totalPrice += shell.ematerialPrice;
+        //console.log(`Production price: ${shell.productionPrice}`);
+        totalPrice += shell.productionPrice;
+        //console.log(`Extra diamond: ${shell.ediamondPrice}`);
+        totalPrice += shell.ediamondPrice;
+        for (const diamond of diamonds) {
+            const diamond_price = await fetchDiamondPrice(diamond.cut, diamond.color, diamond.clarity, diamond.caratWeight, diamond.caratWeight);
+            //console.log(`Diamond: ${diamond_price}`);
+            totalPrice += diamond_price;
+        }
+        for (const material of materials) {
+            const material_price = await fetchMaterialPrice(material.material.materialId);
+            //console.log(`Material: ${material_price}`);
+            totalPrice += material_price;
+        }
+        //console.log(`Markup rate: ${shell.markupRate}`);
+        totalPrice = totalPrice * shell.markupRate;
+        return totalPrice;
+    }
 
     const createOrder = async () => {
 
         if (sessionStorage.getItem("customer_id") === null || productSetting.designId == null || productSetting.shellId == null || productSetting.diamonds.length == 0) {
-            console.log("ERROR");
+            toast.info(`You need to be logged in to place the order!`);
         } else {
             try {
                 const object = {
@@ -62,45 +115,29 @@ const CompleteProduct = () => {
         }
     }
 
-    const getDesign = async () => {
-        try { 
-            //console.log(`GET http://localhost:8080/api/product-designs/${productSetting.designId}`);
-            const response = await axios.get(`http://localhost:8080/api/product-designs/${productSetting.designId}`);
-            if(!response.data || response.status === 204) {
-                console.log('error, cannot fetch, wrong id');
+    const getDiamonds = async () => {
+        try {
+
+            const response = await axios.get(`http://localhost:8080/api/diamonds/all`);
+            if (!response.data || response.status === 204) {
+                console.log('no data found for diamonds');
             } else {
-                setProductDesign(response.data);
-                setShell(response.data.productShellDesigns.find(val => val.productShellDesignId === productSetting.shellId));
-                getDiamonds();
-                getMaterials();
+                const diamonds = response.data.filter(v => productSetting.diamonds.includes(v.diamondId));
+                return diamonds;
             }
+
         } catch (error) {
             console.log(error);
         }
     }
 
-    const getDiamonds = async () => {
-        try {
-
-            const response = await axios.get(`http://localhost:8080/api/diamonds/all`);
-            if(!response.data || response.status === 204) {
-                console.log('no data found for diamonds');
-            } else {
-                setDiamonds(response.data.filter(v => productSetting.diamonds.includes(v.diamondId)));
-            }
-
-        } catch (error) {
-            console.log(error);
-        }
-    } 
-
     const getMaterials = async () => {
         try {
             const response = await axios.get(`http://localhost:8080/api/product-shell-material/${productSetting.shellId}`);
-            if(!response.data || response.status === 204) {
-                
+            if (!response.data || response.status === 204) {
+
             } else {
-                setMaterials(response.data);
+                return response.data;
             }
         } catch (error) {
             console.log(error);
@@ -119,14 +156,14 @@ const CompleteProduct = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col">
-                            <img src={productDesign.designFile} className='img-fluid mx-auto'/>
+                            <img src={productDesign.designFile} className='img-fluid mx-auto' />
                         </div>
                         <div className="col">
                             <h1 className='display-1'>Product preview</h1>
                             <h4 className='fw-bold'>{productDesign.designName} - {shell.shellName}</h4>
                             <h4>Diamonds</h4>
                             <ul>
-                                {diamonds.map(d => 
+                                {diamonds.map(d =>
                                     <li key={d.diamondId}>
                                         {d.diamondCode} - {d.diamondName} - {d.shape} - {d.cut} - {d.clarity} - {d.color} - {d.caratWeight}
                                     </li>
@@ -143,7 +180,12 @@ const CompleteProduct = () => {
                                 }
                             </ul>
                             <h1 className='fw-bold'>Total price</h1>
-                            <h1 className='fw-bold text-success'>XXX,XXX,XXX VND</h1>
+                            <h1 className='fw-bold text-success'>
+                                {estimatedPrice === null
+                                    ? 'Estimating price...'
+                                    : formatPrice(estimatedPrice)
+                                }
+                            </h1>
                             <button className='btn btn-success'>Check out</button>
                         </div>
                     </div>
