538fde0e8f4d9ce65706d73c11e26ad5c78f2320
AnhNTSE183225 <anhntse183225@fpt.edu.vn>
2024-06-10 14:01:34 +0700

    diamond images

--- src/pages/build-your-own/DiamondCard.jsx
+++ src/pages/build-your-own/DiamondCard.jsx
@@ -10,7 +10,7 @@ const DiamondCard = ({ diamond, isSelected, onClick }) => {
     };
     return (
         <div className={`${styles.card} ${isSelected ? styles.selected : ''}`} onClick={() => onClick(diamond.diamondId, !isSelected)}>
-            <img src="https://ion.bluenile.com/sgmdirect/photoID/31377337/Diamond/21512301/nl/Diamond-round-1.02-Carat_3_first_.jpg" className={styles.cardImgTop} alt="diamond" />
+            <img src={diamond.image} className={styles.cardImgTop} alt="diamond" />
             <div className={styles.cardBody}>
                 <h5 className={styles.cardTitle}>{diamond.diamondCode} - {diamond.diamondName}</h5>
                 <p className={styles.cardText}>
