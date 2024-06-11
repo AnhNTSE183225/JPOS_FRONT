6d35ebe2b7d8003b89d7db945e800d3232a9ae6e
Liem <LiemNHTSE183715@fpt.edu.vn>
2024-06-11 21:45:44 +0700

    Login and Register v1

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,9 @@
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Toaster, toast } from "sonner";
 import axios from "axios";
+import styles from '/src/css/LoginPage.module.css';
+
 const LoginPage = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -15,8 +16,6 @@ const LoginPage = () => {
     const handlePassword = (event) => {
         setPassword(event.target.value);
     };
-
-
 
     const login = () => {
         if (username.length !== 0 && password.length !== 0) {
@@ -33,13 +32,13 @@ const LoginPage = () => {
                     navigate("/");
                 })
                 .catch((error) => {
-                    
                     console.log(error);
                 });
         } else {
-            toast.error("Please fill in all forms!");
+            alert("Please fill in all forms!");
         }
     };
+
     const loginStaff = () => {
         if (username.length !== 0 && password.length !== 0) {
             axios
@@ -56,66 +55,60 @@ const LoginPage = () => {
                     navigate("/staff/request");
                 })
                 .catch((error) => {
-                    toast('Invalid login credentials');
+                    alert("Invalid login credentials");
                     console.log(error);
                 });
         } else {
-            toast.error("Please fill in all forms!");
+            alert("Please fill in all forms!");
         }
     };
 
     return (
-        <>
-            <div className="container">
-                <div className="row">
+        <div className={`container-fluid d-flex align-items-center justify-content-center ${styles.fullHeight}`}>
+            <div className={`card p-3 mt-3 ${styles.centerCard}`}>
+                <div className="mb-3">
+                    <h1>Login</h1>
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Username</label>
+                    <textarea
+                        value={username}
+                        onChange={handleUsername}
+                        maxLength={255}
+                        className="form-control"
+                        rows="1"
+                        cols="30"
+                    ></textarea>
+                </div>
+                <div className="mb-3">
+                    <div className="d-flex justify-content-between align-items-center">
+                        <label className="form-label">Password</label>
+                        <Link to="" className={styles.customLink}>Forgot Password?</Link>
+                    </div>
+                    <input
+                        value={password}
+                        onChange={handlePassword}
+                        type="password"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3 row">
                     <div className="col">
-                        <div className="card p-3 mt-3">
-                            <div className="mb-3">
-                                <h1>Login</h1>
-                            </div>
-                            <div className="mb-3">
-                                <label className="form-label">Username</label>
-                                <textarea
-                                    value={username}
-                                    onChange={handleUsername}
-                                    maxLength={255}
-                                    className="form-control"
-                                    rows="1"
-                                    cols="30"
-                                ></textarea>
-                            </div>
-                            <div className="mb-3">
-                                <div className="d-flex justify-content-between align-items-center">
-                                    <label className="form-label">Password</label>
-                                    <Link to="" style={{ color: '#48AAAD' }}>Forgot Password?</Link>
-                                </div>
-                                <input
-                                    value={password}
-                                    onChange={handlePassword}
-                                    type="password"
-                                    className="form-control"
-                                />
-                            </div>
-                            <div className="mb-3 row">
-                                <div className="col">
-                                    <button onClick={login} className="btn btn-dark w-100">
-                                        Login
-                                    </button>
-                                </div>
-                                <div className="col">
-                                    <Link to="/register">
-                                        <button className="btn btn-light w-100">Register</button>
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="d-flex justify-content-end">
-                                <Link to="" onClick={loginStaff} style={{ color: '#48AAAD' }}>Login as staff</Link>
-                            </div>
-                        </div>
+                        <button onClick={login} className="btn btn-dark w-100">
+                            Login
+                        </button>
+                    </div>
+                    <div className="col">
+                        <Link to="/register">
+                            <button className="btn btn-light w-100">Register</button>
+                        </Link>
                     </div>
                 </div>
+                <div className="d-flex justify-content-end">
+                    <Link to="" onClick={loginStaff} className={styles.customLink}>Login as staff</Link>
+                </div>
             </div>
-        </>
+        </div>
     );
 };
 
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,14 +1,13 @@
-import NavigationBar from "../components/NavigationBar";
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import {Toaster, toast} from 'sonner';
 import axios from 'axios';
+import styles from '/src/css/RegisterPage.module.css';
 
 const RegisterPage = () => {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [fullName, setfullName] = useState('');
+    const [fullName, setFullName] = useState('');
     const [address, setAddress] = useState('');
 
     const handlePassword = (event) => {
@@ -20,7 +19,7 @@ const RegisterPage = () => {
     }
 
     const handleFullName = (event) => {
-        setfullName(event.target.value);
+        setFullName(event.target.value);
     }
 
     const handleAddress = (event) => {
@@ -42,52 +41,46 @@ const RegisterPage = () => {
                 }
             ).catch(
                 (error) => {
-                    toast.error('Username already exists!');
+                    alert('Username already exists!');
                 }
             )
         } else {
-            toast.error('Please fill in all fields!');
+            alert('Please fill in all fields!');
         }
     }
 
     return (
-        <>
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <div className='card p-3 mt-3'>
-                            <div className='mb-3'>
-                                <h1>Register</h1>
-                            </div>
-                            <div className='mb-3'>
-                                <label className='form-label'>Username</label>
-                                <textarea value={username} onChange={handleUsername} maxLength={255} className="form-control" rows='1' cols='30'></textarea>
-                            </div>
-                            <div className='mb-3'>
-                                <label className='form-label'>Password</label>
-                                <input value={password} onChange={handlePassword} type="password" className='form-control' />
-                            </div>
-                            <div className='mb-3'>
-                                <label className='form-label'>Name</label>
-                                <textarea value={fullName} onChange={handleFullName} maxLength={255} className="form-control" rows='1' cols='30'></textarea>
-                            </div>
-                            <div className='mb-3'>
-                                <label className='form-label'>Address</label>
-                                <textarea value={address} onChange={handleAddress} maxLength={255} className="form-control" rows='1' cols='30'></textarea>
-                            </div>
-                            <div className='mb-3 row'>
-                                <div className='col'>
-                                    <button onClick={customerRegister} className='btn btn-dark w-100'>Create account</button>
-                                </div>
-                                <div className='col'>
-                                    <Link to='/customer-login'><button className='btn btn-light w-100'>Go to login</button></Link>
-                                </div>
-                            </div>
-                        </div>
+        <div className={`container-fluid d-flex align-items-center justify-content-center ${styles.fullHeight}`}>
+            <div className={`card p-3 mt-3 ${styles.centerCard}`}>
+                <div className='mb-3'>
+                    <h1>Register</h1>
+                </div>
+                <div className='mb-3'>
+                    <label className='form-label'>Username</label>
+                    <textarea value={username} onChange={handleUsername} maxLength={255} className="form-control" rows='1' cols='30'></textarea>
+                </div>
+                <div className='mb-3'>
+                    <label className='form-label'>Password</label>
+                    <input value={password} onChange={handlePassword} type="password" className='form-control' />
+                </div>
+                <div className='mb-3'>
+                    <label className='form-label'>Name</label>
+                    <textarea value={fullName} onChange={handleFullName} maxLength={255} className="form-control" rows='1' cols='30'></textarea>
+                </div>
+                <div className='mb-3'>
+                    <label className='form-label'>Address</label>
+                    <textarea value={address} onChange={handleAddress} maxLength={255} className="form-control" rows='1' cols='30'></textarea>
+                </div>
+                <div className='mb-3 row'>
+                    <div className='col'>
+                        <button onClick={customerRegister} className='btn btn-dark w-100'>Create account</button>
+                    </div>
+                    <div className='col'>
+                        <Link to='/customer-login'><button className='btn btn-light w-100'>Go to login</button></Link>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
