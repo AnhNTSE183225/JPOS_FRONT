9610659502ac11e5b7448d7996e4d7a9673279f5
Nguyen <nguyenmse181820@fpt.edu.vn>
2024-06-11 23:45:17 +0700

    minor change profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,48 +1,41 @@
+import React from 'react';
 import { useNavigate } from "react-router-dom";
+import image from '/src/assets/profile.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ProfilePage = () => {
-
     const navigate = useNavigate();
 
     return (
-        <>
-            <div className="d-flex justify-content-center">
-                <div className="card" style={{ borderRadius: '15px' }}>
-                    <div className="card-body text-center">
-                        <div className="mt-3 mb-4">
-                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
-                                className="rounded-circle img-fluid" style={{ width: '100px' }} alt="Profile" />
-                        </div>
-                        <h4 className="mb-2">{sessionStorage.getItem('name')}</h4>
-                        <p className="text-muted mb-4">@Customer</p>
-
-                        <div className="row">
-                            <div className="col d-flex">
-                                <button type="button" className="btn btn-primary btn-rounded btn-lg">
-                                    Message now
-                                </button>
-                            </div>
-                            <div className="col d-flex">
-                                <button onClick={() => navigate("/profile/your-request")} type="button" className=" btn btn-primary btn-rounded btn-lg">
-                                    View requests
-                                </button>
-                            </div>
-                        </div>
-
-                        <div className="d-flex justify-content-between text-center mt-5 mb-2">
-                            <div>
-                                <p className="mb-2 h5">01</p>
-                                <p className="text-muted mb-0">Quantity purchased</p>
-                            </div>
-                            <div className="px-3">
-                                <p className="mb-2 h5">01</p>
-                                <p className="text-muted mb-0">Quantity PreOrder</p>
-                            </div>
-                        </div>
+        <div className="d-flex justify-content-center align-items-center vh-100">
+            <div className="card shadow" style={{ borderRadius: '15px', maxWidth: '400px', width: '100%' }}>
+                <div className="card-body text-center">
+                    <div className="mt-3 mb-4">
+                        <img src={image} className="rounded-circle img-fluid" style={{ width: '100px' }} alt="Profile" />
+                    </div>
+                    <h4 className="mb-2 text-uppercase">{sessionStorage.getItem('name')}</h4>
+                    <p className="text-muted mb-4">Customer</p>
+                    <div className="d-flex flex-column">
+                        <button 
+                            type="button" 
+                            className="btn btn-lg mb-2" 
+                            style={{ backgroundColor: '#48AAAD', color: '#fff' }}
+                            onClick={() => navigate("/profile/order-details")}
+                        >
+                            View Order Detail
+                        </button>
+                        <button 
+                            type="button" 
+                            className="btn btn-lg" 
+                            style={{ backgroundColor: '#48AAAD', color: '#fff' }}
+                            onClick={() => navigate("/profile/your-request")}
+                        >
+                            View Request
+                        </button>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
