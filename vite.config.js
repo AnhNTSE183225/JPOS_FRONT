ac6991ffa8f5e8a0167fb1ab3b3175b34d62e303
AnhNTSE183225 <anhntse183225@fpt.edu.vn>
2024-06-23 18:07:02 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    historyApiFallback: true,
-  },
-  preview: {
-    historyApiFallback: true,
-  }
+  plugins: [react()]
 })
