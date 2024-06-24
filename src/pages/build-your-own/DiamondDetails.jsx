c123b5ca08d9a4e4bb3d908716e9881a973051bc
Nguyen <nguyenmse181820@fpt.edu.vn>
2024-06-24 18:56:22 +0700

    design diamond details

--- src/pages/build-your-own/DiamondDetails.jsx
+++ src/pages/build-your-own/DiamondDetails.jsx
@@ -12,9 +12,9 @@ const DiamondDetails = () => {
     const { diamondId } = useParams();
     const [diamond, setDiamond] = useState(null);
     const [diamondPrice, setDiamondPrice] = useState(null);
-    
+
     const navigate = useNavigate();
-    
+
 
     useEffect(() => {
         if (sessionStorage.getItem('designId') === null) {
@@ -48,7 +48,7 @@ const DiamondDetails = () => {
                 //console.log('Fetched Diamond Data:', response.data);
                 setDiamond(result);
                 setDiamondPrice(diamondPrice);
-                
+
             }
         } catch (error) {
             console.error('An error occurred while fetching the diamond data:', error);
@@ -111,75 +111,82 @@ const DiamondDetails = () => {
     if (diamond === null || diamondPrice == null) {
         return <div>Loading...</div>;
     } else {
-        
+
         return (
-            <div className={styles.container}>
-                <div className={`${styles[`imageSection`]} col-md-5`}>
-                    <img src={diamond.image} alt="Diamond" className={styles.diamondImage} />
-                    <div className={styles.specs}>
-                        <p className={styles.specItem}><span>Shape:</span><span>{diamond.shape}</span></p>
-                        <p className={styles.specItem}><span>Cut:</span><span>{diamond.cut}</span></p>
-                        <p className={styles.specItem}><span>Color:</span><span>{diamond.color}</span></p>
-                        <p className={styles.specItem}><span>Clarity:</span><span>{diamond.clarity}</span></p>
-                        <p className={styles.specItem}><span>Carat Weight:</span><span>{diamond.caratWeight}</span></p>
-                    </div>
+            <>
+                <div className='container'>
+                    <div className={styles.container}>
+                        <div className={styles.content}>
+                            <div className={`${styles[`imageSection`]} col-md-6`}>
+                                <img src={diamond.image} alt="Diamond" className={styles.diamondImage} />
+                                <div className={styles.specs}>
+                                    <p className={styles.specItem}><span>Shape:</span><span>{diamond.shape.charAt(0).toUpperCase() + diamond.shape.slice(1)}</span></p>
+                                    <p className={styles.specItem}><span>Cut:</span><span>{diamond.cut}</span></p>
+                                    <p className={styles.specItem}><span>Color:</span><span>{diamond.color}</span></p>
+                                    <p className={styles.specItem}><span>Clarity:</span><span>{diamond.clarity}</span></p>
+                                    <p className={styles.specItem}><span>Carat Weight:</span><span>{diamond.caratWeight}</span></p>
+                                </div>
 
-                </div>
-                <div className={`${styles[`detailsSection`]} col-md-7`}>
-                    <h1 className={styles.diamondTitle}>{diamond.diamondName}</h1>
-                    <div>
-                        <p className={styles.price}>{formatPrice(diamondPrice)} (Diamond Price)</p>
+                            </div>
+                            <div className={`${styles[`detailsSection`]} col-md-6`}>
+                                <p className={`fs-2 ${styles.diamondTitle}`}>{diamond.diamondName}</p>
+                                <p className={`fs-4 ${styles.diamondTitle}`}>{diamond.origin.charAt(0).toUpperCase() + diamond.origin.slice(1).toLowerCase().replace("_", " ")} {diamond.caratWeight} {diamond.color}-{diamond.clarity} {diamond.cut.replace("_", " ")} Cut {diamond.shape} Diamond</p>
+                                <div>
+                                    <p className={`mb-3 ${styles.price}`}>{formatPrice(diamondPrice)} (Diamond Price)</p>
+                                </div>
+                                <div className={`mt-4 mb-5 ${styles.option}`}>
+                                    <b>Flexible Payment Options:</b> 3 Interest-Free Payments of {formatPrice(diamondPrice / 3)}
+                                </div>
+                                <div className={`mb-3 ${styles.buttonsSection}`}>
+                                    <div className='row'>
+                                        <div className='col'>
+                                            {
+                                                isSelected()
+                                                    ? <button onClick={removeSelection} className={styles.removeButton}>Remove selection</button>
+                                                    : <button onClick={selectDiamond} className={styles.primaryButton}>Select this diamond</button>
+                                            }
+                                        </div>
+                                        <div className='col'>
+                                            <button className={styles.secondaryButton}>Consult an expert</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={`row ${styles.paymentOptions}`}>
+                                    <p className="fs-5">Your order includes:</p>
+                                    <div className={styles.optionBox}>
+                                        <div className={styles.iconContainer}>
+                                            <FontAwesomeIcon icon={faTruckFast} />
+                                        </div>
+                                        <div className={styles.textContainer}>
+                                            <p><b>Free Shipping</b></p>
+                                            <p>We're committed to making your entire experience a pleasant one, from shopping to shipping.</p>
+                                        </div>
+                                    </div>
+                                    <div className={styles.optionBox}>
+                                        <div className={styles.iconContainer}>
+                                            <FontAwesomeIcon icon={faChartBar} />
+                                        </div>
+                                        <div className={styles.textContainer}>
+                                            <p><b>Appraisal Included</b></p>
+                                            <p>An appraisal is a document stating the approximate monetary value of your item. It is only available for items over $800.</p>
+                                        </div>
+                                    </div>
+                                    <div className={styles.optionBox}>
+                                        <div className={styles.iconContainer}>
+                                            <FontAwesomeIcon icon={faRightLeft} />
+                                        </div>
+                                        <div className={styles.textContainer}>
+                                            <p><b>Free Returns</b></p>
+                                            <p>Our commitment to you does not end at delivery. We offer free returns (U.S and Canada) to make your experience as easy as possible.</p>
+                                        </div>
+                                    </div>
+                                </div>
 
-                    </div>
-                    <div className='mt-3 ms-3'>
-                        <p>Flexible Payment Options: 3 Interest-Free Payments of {formatPrice(diamondPrice / 3)}</p>
-                    </div>
-                    <div className={styles.paymentOptions}>
-                        <h3>Your order included: </h3>
-                        <div className={styles.optionBox}>
-                            <div className={styles.iconContainer}>
-                                <FontAwesomeIcon icon={faTruckFast} />
-                            </div>
-                            <div className={styles.textContainer}>
-                                <h3>Free Shipping</h3>
-                                <p>We're committed to making your entire experience a pleasant one, from shopping to shipping.</p>
-                            </div>
-                        </div>
-                        <div className={styles.optionBox}>
-                            <div className={styles.iconContainer}>
-                                <FontAwesomeIcon icon={faChartBar} />
-                            </div>
-                            <div className={styles.textContainer}>
-                                <h3>Appraisal Included</h3>
-                                <p>An appraisal is a document stating the approximate monetary value of your item. It is only available for items over $800.</p>
-                            </div>
-                        </div>
-                        <div className={styles.optionBox}>
-                            <div className={styles.iconContainer}>
-                                <FontAwesomeIcon icon={faRightLeft} />
-                            </div>
-                            <div className={styles.textContainer}>
-                                <h3>Free Returns</h3>
-                                <p>Our commitment to you does not end at delivery. We offer free returns (U.S and Canada) to make your experience as easy as possible.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.buttonsSection}>
-                        <div className='row'>
-                            <div className='col'>
-                                {
-                                    isSelected()
-                                        ? <button onClick={removeSelection} className={styles.removeButton}>Remove selection</button>
-                                        : <button onClick={selectDiamond} className={styles.primaryButton}>Select this diamond</button>
-                                }
-                            </div>
-                            <div className='col'>
-                                <button className={styles.secondaryButton}>Consult an expert</button>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </>
         );
     }
 };
